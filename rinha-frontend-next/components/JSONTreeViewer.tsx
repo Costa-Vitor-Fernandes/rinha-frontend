319379c3894ed04d9b5a9cc3d8e6f5c802419cfb
VitorCosta <Costa-Vitor-Fernandes>
2023-10-18 14:52:01 -0300

    implements Load More button for large arrays

--- rinha-frontend-next/components/JSONTreeViewer.tsx
+++ rinha-frontend-next/components/JSONTreeViewer.tsx
@@ -1,6 +1,6 @@
 import { inter7 } from "@/pages"
 import { Inter } from 'next/font/google'
-import React,{ useState, useEffect , useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 interface JSONTreeViewerProps {
     data: any;
@@ -10,37 +10,35 @@ interface JSONTreeViewerProps {
 
 const JSONTreeViewer: React.FC<JSONTreeViewerProps> = ({ data, fileName, size }) => {
     const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
-    
+
     const toggleExpand = (key: string) => {
         setExpandedKeys(prevKeys => (
             prevKeys.includes(key)
-            ? prevKeys.filter(k => k !== key)
-            : [...prevKeys, key]
-            ));
-        };
-        const handleKeyDown = (event: React.KeyboardEvent, key:any) => {
-    
-            if (event.key === 'Enter') {
-                toggleExpand(key);
-            }
-        };
-        
+                ? prevKeys.filter(k => k !== key)
+                : [...prevKeys, key]
+        ));
+    };
+    const handleKeyDown = (event: React.KeyboardEvent, key: any) => {
+
+        if (event.key === 'Enter') {
+            toggleExpand(key);
+        }
+    };
+
     const renderNode = (key: any, value: any, parentKey: string) => {
         const fullKey = parentKey ? `${parentKey}.${key}` : key;
         const isExpanded = expandedKeys.includes(fullKey);
-        
-        
+
+
 
         if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
             return (
-                <div className="mx-5 py-2" 
-                key={key}
-               
-    
+                <div className="mx-5 py-2"
+                    key={key}
                 >
-                    <span className="text-gray-400"   
-                     onClick={() => toggleExpand(fullKey)}
-                     role="button" tabIndex={0} onKeyDown={(e)=>handleKeyDown(e,fullKey)} >
+                    <span className="text-gray-400"
+                        onClick={() => toggleExpand(fullKey)}
+                        role="button" tabIndex={0} onKeyDown={(e) => handleKeyDown(e, fullKey)} >
                         {key}:
                     </span>
                     {isExpanded && (
@@ -54,16 +52,16 @@ const JSONTreeViewer: React.FC<JSONTreeViewerProps> = ({ data, fileName, size })
 
         if (typeof value === 'object' && Array.isArray(value) && value !== null) {
             return (
-                <div className="mx-5" 
-                key={key}>
-                    <span className="text-[#4e9590]" role="button" tabIndex={0} onKeyDown={(e)=>handleKeyDown(e,fullKey)} onClick={() => toggleExpand(fullKey)}>
+                <div className="mx-5"
+                    key={key}>
+                    <span className="text-[#4e9590]" role="button" tabIndex={0} onKeyDown={(e) => handleKeyDown(e, fullKey)} onClick={() => toggleExpand(fullKey)}>
                         {key}:
                     </span>
-                        <span className="text-orange-400">[</span>
+                    <span className="text-orange-400">[</span>
                     {isExpanded && (
-                            <div className="border-l-2 border-gray-300">
-                                {renderArray(value, fullKey)}
-                            </div>
+                        <div className="border-l-2 border-gray-300">
+                            {renderArray(value, fullKey)}
+                        </div>
                     )}
                     <span className="text-orange-400">]</span>
                 </div>
@@ -84,32 +82,62 @@ const JSONTreeViewer: React.FC<JSONTreeViewerProps> = ({ data, fileName, size })
     };
 
     const renderObject = (obj: any, parentKey: string) => {
-        return Object.entries(obj).map(([key, value], index) =>
+        if(!Array.isArray(obj))
+        return Object.entries(obj).map(([key, value]) =>
             renderNode(key, value, parentKey)
         );
+        else{
+            return renderArray(obj,parentKey)
+        }
     };
 
-    const isArraySmall = (array:any[]):any[]|false =>{
-        const threshold = 3000
-    if (Array.isArray(array) && array.length < threshold) {
-        return array;
+    const [sliceIndex,setSliceIndex] = useState<number>(0)
+
+
+    const loadMore = ()=>{
+        // console.log('load more', sliceIndex)
+        setSliceIndex((prev=>prev+1))
     }
 
-    return false;
+
+    const isArraySmall = (array: any[]): any[] | false => {
+        const threshold = 30
+        if (Array.isArray(array) && array.length < threshold) {
+            return array;
+        }
+
+        return false;
     }
 
     const renderArray = (arr: any[], parentKey: string) => {
-    if(isArraySmall(arr)){
-        return arr.map((v, i) =>
-        renderNode(i, v, parentKey)
-        );
+        // console.log('rendering array')
+        if (isArraySmall(arr)) {
+            return arr.map((v, i) =>
+                renderNode(i, v, parentKey)
+            );
+        }
+        else {
+            // console.log('array is too big to render')
+            const itemsDisplayed = (sliceIndex+1)*50
+            let slicedArray = arr
+            if(itemsDisplayed < arr.length){
+                slicedArray = arr.slice(sliceIndex,itemsDisplayed)
+            }
+            return (
+                <div>
+                     {slicedArray.map((v, i) =>
+                         renderNode(i, v, parentKey)
+                     )}
+                     {slicedArray.length < arr.length &&
+                         <button className="m-2 rounded-md border-2 bg-gradient-to-b from-gray-200 to-gray-100 border-zinc-950 px-1 py-2 hover:from-gray-100 focus:bg-gray-50 focus:cursor-pointer hover:cursor-pointer" onClick={loadMore}>Load More</button>
+                     }
+                </div>
+            );
+        }
     }
-    else{
-        console.log('array is too big to render')    
-    }
-    }
+    
     return (
-        <div className="flex flex-col max-w-[100vw]">
+        <div className="flex flex-col min-w-[100vw]">
             <div className='self-center'>
                 <h1 className={`text-4xl ${inter7.className} py-3 `}>{fileName}</h1>
                 <hr className="py-1"></hr>
@@ -122,95 +150,3 @@ const JSONTreeViewer: React.FC<JSONTreeViewerProps> = ({ data, fileName, size })
 };
 
 export default JSONTreeViewer;
-
-// const findLargeArray = (data: any, threshold: number): any[] | null => {
-//     if (Array.isArray(data) && data.length > threshold) {
-//         return data;
-//     }
-
-//     if (data && typeof data === 'object') {
-//         for (const key in data) {
-//             const result = findLargeArray(data[key], threshold);
-//             if (result) return result;
-//         }
-//     }
-
-//     return null;
-// };
-
-// const JSONTreeViewerBIG: React.FC<JSONTreeViewerProps> = ({ data, fileName }) => {
-//     console.log(data)
-//     const [currentPage, setCurrentPage] = useState(1);
-//     const itemsPerPage = 10; // Adjust this value as needed
-//     const threshold = 10000; // Adjust this threshold as needed
-
-//     const largeArray = findLargeArray(data, threshold);
-//     const paginatedFeatures = largeArray ? largeArray.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage) : [];
-
-//     const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
-
-//     const toggleExpand = (key: string) => {
-//         setExpandedKeys(prevKeys => (
-//             prevKeys.includes(key)
-//                 ? prevKeys.filter(k => k !== key)
-//                 : [...prevKeys, key]
-//         ));
-//     };
-
-//     const renderNode = (key: string, value: any) => {
-//         const isObject = typeof value === 'object' && !Array.isArray(value);
-
-//         return (
-//             <div key={key} style={{ marginLeft: '20px' }}>
-//                 <span
-//                     style={{ cursor: isObject ? 'pointer' : 'default', color: isObject ? '#4e9590' : 'black' }}
-//                     onClick={() => isObject && toggleExpand(key)}
-//                 >
-//                     {isObject ? (expandedKeys.includes(key) ? '[-] ' : '[+] ') : ''}{key}: {isObject ? '' : JSON.stringify(value)}
-//                 </span>
-//                 {isObject && expandedKeys.includes(key) && (
-//                     <div>{renderObject(value)}</div>
-//                 )}
-//             </div>
-//         );
-//     };
-
-//     const renderObject = (obj: any) => {
-//         return Object.entries(obj).map(([key, value]) => renderNode(key, value));
-//     };
-
-//     return (
-//         <div>
-//             <h1 className={`text-xl`}>{fileName}</h1>
-//             <hr></hr>
-//             {/* largeArray(data, threshold)? : */}
-//             {paginatedFeatures && paginatedFeatures.map((item, index) => (
-//                 <div key={index}>
-//                     {renderNode(`Item ${index}`, item)}
-//                 </div>
-//             ))}
-//             <div>
-//                 <button
-//                     onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-//                     disabled={currentPage === 1}
-//                 >
-//                     Previous Page
-//                 </button>
-//                 {largeArray && <button
-//                     onClick={() => {
-//                         const nextPage = Math.min(
-//                             currentPage + 1,
-//                             Math.ceil(largeArray.length / itemsPerPage)
-//                         );
-//                         setCurrentPage(nextPage);
-//                     }}
-//                     disabled={currentPage * itemsPerPage >= largeArray.length}
-//                 >
-//                     Next Page
-//                 </button>}
-//             </div>
-//         </div>
-//     );
-// };
-
-
